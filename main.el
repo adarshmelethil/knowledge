8242583c36d79b220b7a8dde6cde8782ecbda022
adarshmelethil <adarshmelethil@gmail.com>
2022-04-16 16:14:13 -0600

    add notes on taking notes

---
+++ main.el
@@ -0,0 +1,51 @@
+;;; main.el --- Description -*- lexical-binding: t; -*-
+;;
+;; Copyright (C) 2022 Adarsh Melethil
+;;
+;; Author: Adarsh Melethil <adarshmelethil@gmail.com>
+;; Maintainer: Adarsh Melethil <adarshmelethil@gmail.com>
+;; Created: April 16, 2022
+;; Modified: April 16, 2022
+;; Version: 0.0.1
+;; Keywords: abbrev bib c calendar comm convenience data docs emulations extensions faces files frames games hardware help hypermedia i18n internal languages lisp local maint mail matching mouse multimedia news outlines processes terminals tex tools unix vc wp
+;; Homepage: https://github.com/adarshmelethil/main
+;; Package-Requires: ((emacs "24.3"))
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; Commentary:
+;;
+;;  Description
+;;
+;;; Code:
+
+
+(require 'datetime)
+
+
+(Linx)
+(Guix)
+(Bootloader
+  :link "https://www.win.tue.nl/~aeb/partitions/partition_types-2.html"
+  ((time "1995")
+   "here")
+  ((time "2000")
+   "this")
+  )
+
+(Encoding
+ ((Description logics)
+  (:description
+   "logic or langauge to descripbe collections and relations betweeen collections")))
+
+(Mint
+ ((time "2010")
+  (bootloader)))
+
+
+(defmacro bootloader []
+  )
+
+
+(provide 'main)
+;;; main.el ends here
